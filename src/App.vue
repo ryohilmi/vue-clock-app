89452056ca1865b15ecaba752170cebadefa9ba7
ryohilmi <ryodm.74@gmail.com>
2022-09-12 19:07:56 +0700

    feat: add clock component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
-<script setup></script>
+<script setup>
+import Clock from './components/Clock.vue';
+</script>
 
-<template>Hello World</template>
+<template>
+  <Clock />
+</template>
 
-<style scoped></style>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500&family=PT+Serif:wght@400;700&family=Poppins:wght@100;200;300;400;500;600;700&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  background: rgb(15, 20, 30);
+  color: white;
+  font-family: 'Poppins';
+}
+
+.clock {
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Clock.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <div class="clock">
+    <div class="time">19:00:00</div>
+    <div class="date">Senin, 12 September 2022</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.clock {
+  text-align: center;
+  padding-top: 15em;
+
+  .time {
+    font-size: 4rem;
+    font-weight: 500;
+  }
+
+  .date {
+    font-weight: 300;
+    color: rgb(192, 192, 192);
+  }
+}
+</style>
