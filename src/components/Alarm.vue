0e12fa8d127d42c1c96024ea4f9bc76af91f5a19
Ryo Hilmi <ryodm.74@gmail.com>
2022-09-29 18:55:43 +0700

    adjust alarm item styling

--- src/components/Alarm.vue
+++ src/components/Alarm.vue
@@ -1,36 +1,40 @@
 <script>
-import AlarmItem from "./AlarmItem.vue"
+import AlarmItem from "./AlarmItem.vue";
 
 export default {
-    data() {
-        return {
-            alarms: [
-                {hour: 7, minute: 0, isActive: false},
-                {hour: 7, minute: 0, isActive: true}
-            ]
-        }
-    },
-    components: {
-        AlarmItem
-    }
-}
+  data() {
+    return {
+      alarms: [
+        { hour: 7, minute: 0, isActive: false },
+        { hour: 7, minute: 0, isActive: true },
+      ],
+    };
+  },
+  components: {
+    AlarmItem,
+  },
+};
 </script>
 
 <template>
-<div class="alarm">
-    <AlarmItem v-for="(alarm, index) in alarms" :key="index" :alarmId="index" :alarm="alarm"/>
-</div>
+  <div class="alarm">
+    <AlarmItem
+      v-for="(alarm, index) in alarms"
+      :key="index"
+      :alarmId="index"
+      :alarm="alarm"
+    />
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .alarm {
-    min-width: 300px;
-    max-width: 40%;
-    margin: 4rem auto;
-    padding: 1rem;
-    background: red;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  min-width: 300px;
+  max-width: 50%;
+  margin: 4rem auto;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
--- src/components/AlarmItem.vue
+++ src/components/AlarmItem.vue
@@ -1,72 +1,95 @@
 <script>
 export default {
-    props: {
-        alarmId: Number,
-        alarm: {
-            type: Object
-        }
+  props: {
+    alarmId: Number,
+    alarm: {
+      type: Object,
     },
-    methods: {
-        log(e) {
-            console.log(e.target.checked);
-        }
-    }
-}
+  },
+  methods: {
+    log(e) {
+      console.log(e.target.checked);
+    },
+  },
+};
 </script>
 
 <template>
-    <div class="alarm-item">
-        <span>{{ alarm.hour }}:{{ alarm.minute.toString().padStart(2, '0') }}</span>
-        <input type="checkbox" name="" :id="alarmId" @change="log($event)" :checked="alarm.isActive">
-        <label :for="alarmId"></label>
-    </div>
+  <div class="alarm-item">
+    <span>{{ alarm.hour }}:{{ alarm.minute.toString().padStart(2, "0") }}</span>
+    <input
+      type="checkbox"
+      name=""
+      :id="alarmId"
+      @change="log($event)"
+      :checked="alarm.isActive"
+    />
+    <label :for="alarmId"></label>
+  </div>
 </template>
 
 <style scoped>
 .alarm-item {
-    background: white;
-    width: 90%;
+  background: white;
+  width: 100%;
+  background-color: rgb(248, 250, 251);
+  box-shadow: -4px -2px 4px 0px #ffffff,
+    4px 2px 6px 0px rgba(209, 217, 230, 0.9);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 1rem auto;
+  padding: 1rem 3rem;
+  border-radius: 20px;
+  /* font-family: "Poppins"; */
+  font-size: 2.2rem;
+  font-weight: 500;
+  color: rgb(57, 92, 105);
 }
 
 input {
-	height: 0;
-	width: 0;
-	visibility: hidden;
+  height: 0;
+  width: 0;
+  visibility: hidden;
 }
 
 label {
-	cursor: pointer;
-	text-indent: -9999px;
-	width: 200px;
-	height: 100px;
-	background: grey;
-	display: block;
-	border-radius: 100px;
-	position: relative;
+  cursor: pointer;
+  text-indent: -9999px;
+  width: 60px;
+  height: 30px;
+  display: block;
+  border-radius: 100px;
+  position: relative;
+  box-shadow: -8px -4px 8px 0px rgba(231, 231, 231, 0.2),
+    8px 4px 12px 0px rgba(84, 84, 85, 0.2),
+    4px 4px 4px 0px rgba(84, 84, 85, 0.2) inset,
+    -4px -4px 4px 0px rgba(231, 231, 231, 0.2) inset;
 }
 
 label:after {
-	content: '';
-	position: absolute;
-	top: 5px;
-	left: 5px;
-	width: 90px;
-	height: 90px;
-	background: #fff;
-	border-radius: 90px;
-	transition: 0.3s;
+  content: "";
+  position: absolute;
+  top: 0px;
+  left: 0;
+  width: 30px;
+  height: 30px;
+  background: #fff;
+  border-radius: 50%;
+  transition: 0.3s;
+  border-radius: 15px;
 }
 
 input:checked + label {
-	background: #bada55;
+  background: rgb(101, 99, 253);
 }
 
 input:checked + label:after {
-	left: calc(100% - 5px);
-	transform: translateX(-100%);
+  left: calc(100%);
+  transform: translateX(-100%);
 }
 
 label:active:after {
-	width: 130px;
+  width: 30px;
 }
 </style>
